f32dbe6c1bb6c29a6f83e98cb85c8c8fef8c2126
DawsonTheroux <dawsontheroux@gmail.com>
2022-12-06 11:59:49 -0500

    Updated President Net shape

--- PresidentNeuralNet.py
+++ PresidentNeuralNet.py
@@ -6,41 +6,47 @@ import torch.nn.functional as F
 class PresidentNet(nn.Module):
     def __init__(self):
         super(PresidentNet, self).__init__()
-        self.dropout = nn.Dropout(0.5)
+        self.dropout0 = nn.Dropout(0.2)
+        self.dropout1 = nn.Dropout(0.5)
+        self.dropout2 = nn.Dropout(0.5)
+        self.dropout3 = nn.Dropout(0.5)
+        self.dropout4 = nn.Dropout(0.5)
+
         self.layer1 = nn.Sequential(
-            nn.Linear(168,504),
+            nn.Linear(168,1024),
             nn.ELU()
         )
         self.layer2 = nn.Sequential(
             #nn.Dropout(0.5),
-            nn.Linear(504, 504), 
+            nn.Linear(1024, 1024), 
             nn.ELU()
         )
         self.layer3 = nn.Sequential(
             #nn.Dropout(0.5),
-            nn.Linear(504,256),
+            nn.Linear(1024,512),
             nn.ELU()
         )
         self.layer4 = nn.Sequential(
             #nn.Dropout(0.5),
-            nn.Linear(256, 128),
+            nn.Linear(512, 256),
             nn.Tanh()
             
         )
         self.layer5 = nn.Sequential(
-            nn.Linear(128,55)
+            nn.Linear(256,55)
         )
        
 
     def forward(self, x):
+        x = self.dropout0(x)
         x = self.layer1(x)
-        x = self.dropout(x)
+        x = self.dropout1(x)
         x = self.layer2(x)
-        x = self.dropout(x)
+        x = self.dropout2(x)
         x = self.layer3(x)
-        x = self.dropout(x)
+        x = self.dropout3(x)
         x = self.layer4(x)
-        x = self.dropout(x)
+        x = self.dropout4(x)
         x = self.layer5(x)
         output = F.log_softmax(x, dim=0)
         return output
